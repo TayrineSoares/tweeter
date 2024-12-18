201d9bedc8e5258a49077ce408ed4d04a3b7c3a3
TayrineSoares <tayrinecristina@hotmail.com>
2024-12-18 11:23:31 -0500

    Add character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+
+
+  $("#tweet-text").on('input', function() {
+    // Get the current value of the textarea and its length
+    const inputLength = $(this).val().length; // Use $(this) to leverage jQuery functions
+    const charCount = 140 - inputLength; 
+
+    // Traverse the DOM to find the associated '.counter' element
+    const counter = $(this).closest("form").find("#counter");
+    // Update the counter text
+    counter.text(charCount);
+
+    // Add a class to indicate when the count is negative
+    if (charCount < 0) {
+      counter.addClass("negative");
+    } else {
+      counter.removeClass("negative");
+    }
+
+  });
+
+});
+
