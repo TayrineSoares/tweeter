64155e093ce15735d0e7c5bbc29cf801c111b232
TayrineSoares <tayrinecristina@hotmail.com>
2024-12-19 19:14:53 -0500

    Create loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -25,31 +25,6 @@ $(document).ready(function() {
     });
   });
 
-    // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
 function createTweetElement(tweetData) {
 
@@ -99,8 +74,24 @@ const renderTweets = function(tweets) {
   }
 };
 
-renderTweets(data);
 
+  // Function to fetch tweets from the server
+  function loadTweets() {
+    $.ajax({
+      type: 'GET',              // HTTP method
+      url: '/tweets',           // Endpoint to fetch tweets
+      dataType: 'json',         // Expect JSON response
+      success: function(tweets) {
+        console.log('Tweets fetched successfully:', tweets);
+        renderTweets(tweets);   // Display fetched tweets
+      },
+      error: function(error) {
+        console.error('Error fetching tweets:', error);
+      }
+    });
+  }
 
+  // Call the loadTweets function to fetch tweets on page load
+  loadTweets();
 
 });
